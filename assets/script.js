2ba788e21aeca6da40dcff83ef9675203abefef3
hariom2008h <tubecreatorhariom@gmail.com>
2025-08-08 18:34:19 +0530

    Delete assets/script.js

--- assets/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const menuToggle = document.getElementById("menu-toggle");
-  const navLinks = document.getElementById("navbar");
-
-  menuToggle.addEventListener("click", () => {
-    navLinks.classList.toggle("show");
-  });
-});
